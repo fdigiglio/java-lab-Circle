18f8e3d12fa2bbc760ede093c4580b0ecdd1c4b3
fdigiglio <fdigiglio22109@columbushs.com>
2020-09-30 13:56:13 -0400

    Final Circle Lab

--- Circle.java
+++ Circle.java
@@ -1,3 +1,48 @@
+import java.lang.Math;
 public class Circle {
     
+    //Instance Varibles
+    private double radiusOfCircle;
+    final double PI = 3.14159;
+
+
+    public Circle(){
+        //Default values
+    }
+
+    
+    //Constructor
+    public Circle(double pRadiusOfCircle){
+        this.radiusOfCircle = pRadiusOfCircle;
+    }
+
+    //Getters and Setters
+    public void setRadius(double newRadius){
+        this.radiusOfCircle = newRadius;
+    }
+
+    //Returns the radius of circle
+    public double getRadius(){
+        return this.radiusOfCircle;
+    }
+    
+    //Returns the diameter
+    public double getDiamater(){
+        return this.radiusOfCircle * 2;
+    }
+
+    //Returns the area of the circle
+    public double getArea(){
+        return Math.pow(this.radiusOfCircle, 2) * PI;
+        
+    }
+
+    //Return the circumference
+    public double getCircumfernce(){
+        return this.radiusOfCircle * PI * 2;
+    }
+
+    
+
+
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.text.DecimalFormat;
+
 public class Main{
     public static void main(String[] args){
-        
+
+        DecimalFormat df = new DecimalFormat("###.##");
+        Circle churcle = new Circle(4);
+        System.out.println("The cicle's area is " + df.format(churcle.getArea()));
+        System.out.println("The circle's diameter is " + churcle.getDiamater());
+        System.out.println("The circle's circumference is " + df.format(churcle.getCircumfernce()));
     }
 }
